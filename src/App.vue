e155ab75521c6ef9fcc5a728671bdf4b54c4fb52
liufan <785454405@qq.com>
2019-05-06 16:13:13 +0800

    初版

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <!-- <a-button>点击</a-button>
+    <div id="nav">
+      <router-link to="/dashboard/analysis">dashboard</router-link> |
+      <router-link to="/form">from</router-link>
+    </div> -->
+    <router-view />
+  </div>
+</template>
+
+<style lang="less">
+
+</style>
